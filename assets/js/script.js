b8c0bd7ce86a3614bae0c5716d3b5d486ca6cbd2
Dillon Kim <dillonkim@Dillons-MacBook-Air.local>
2022-10-28 04:34:42 -0700

    add function to show relative time ontext areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,36 @@ var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var currentDate = moment();
 console.log(currentDate);
 
+function compareHour(hour) {
+    console.log(currentDate.hour())
+    if (currentDate.hour() > hour) {
+        return "past";
+    } else if (currentDate.hour() === hour) {
+        return "present";
+    } else {
+        return "future";
+    }
+}
 
 
 for (var i = 0; i < hoursArray.length; i++){
     var wrapper = document.createElement("div");
     wrapper.setAttribute("class", "row time-block")
     
+    console.log(compareHour(hoursArray[i])); //get past, present, or future
+    wrapper.classList.add(compareHour(hoursArray[i]));
+
     var textbox = document.createElement("textarea");
     textbox.setAttribute("class", "col-md-10 description");
 
+    
+    
+    
     var hour = document.createElement("div");
     hour.textContent = moment(hoursArray[i], 'HH').format('H A')
     hour.setAttribute("class", "col-md-1 hour")
 
-
-
+    
 
     wrapper.appendChild(hour);
     wrapper.appendChild(textbox);
