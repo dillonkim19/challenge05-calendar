e4bf6c18f4ec0371cf455fc7b706cc02b72edadf
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2022-10-30 07:32:56 -0700

    add save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,49 @@
 var container = $(".container");
+var currentDay = $("#currentDay");
 
-var hoursArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var currentDate = moment();
-console.log(currentDate);
+var hoursArray = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 
+
+
+var current = moment();
+// console.log(current);
+
+// return past present or future after comparing with current time
 function compareHour(hour) {
-    console.log(currentDate.hour())
-    if (currentDate.hour() > hour) {
+    // console.log(current.hour())
+    if (current.hour() > hour) {
         return "past";
-    } else if (currentDate.hour() === hour) {
+    } else if (current.hour() === hour) {
         return "present";
     } else {
         return "future";
     }
 }
 
+//add current day to page
+currentDay.text(moment().format('dddd, MMMM Do YYYY'))
 
+// make table with columsn of hour, text, save button, etc
 for (var i = 0; i < hoursArray.length; i++){
     var wrapper = document.createElement("div");
     wrapper.setAttribute("class", "row time-block")
     
-    console.log(compareHour(hoursArray[i])); //get past, present, or future
+    // console.log(compareHour(hoursArray[i])); //get past, present, or future
     wrapper.classList.add(compareHour(hoursArray[i]));
-
-    var textbox = document.createElement("textarea");
-    textbox.setAttribute("class", "col-md-10 description");
-
-    
-    
     
     var hour = document.createElement("div");
     hour.textContent = moment(hoursArray[i], 'HH').format('H A')
     hour.setAttribute("class", "col-md-1 hour")
 
-    
+    var textbox = document.createElement("textarea");
+    textbox.setAttribute("class", "col-md-10 description");
+    // textbox.textContent = "whatever"
 
+    var saveButton = document.createElement("i");
+    saveButton.setAttribute("class", "col-md-1 fas fa-save saveBtn");
+    
+    container.append(wrapper);
     wrapper.appendChild(hour);
     wrapper.appendChild(textbox);
-    container.append(wrapper);
+    wrapper.appendChild(saveButton);
 }
